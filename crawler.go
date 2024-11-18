29bf3ca9310bf70a58587c86f811181868d77b44
maniakalen <pesho10@yahoo.com>
2024-11-18 18:29:48 +0200

    Workers-Way-v1

--- crawler.go
+++ crawler.go
@@ -2,10 +2,12 @@
 package crawler
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -21,6 +23,7 @@ type Crawler struct {
 	urlsChan     chan url.URL
 	channels     Channels
 	scanParent   bool
+	mu           sync.Mutex
 }
 
 // Page is a struct that carries the scanned url, response and response body string
@@ -32,6 +35,30 @@ type Page struct {
 
 // Channels is a Page channels map where the index is the response code so we can define different behavior for the different resp codes
 type Channels map[int]chan Page
+
+var n int = runtime.GOMAXPROCS(0) / 2
+var wg sync.WaitGroup
+
+func worker(c *Crawler, ctx context.Context) {
+	defer fmt.Println("Worker closing")
+	defer wg.Done()
+	for {
+		select {
+		case url := <-c.urlsChan:
+			url, err := c.repairUrl(&url)
+			if err == nil && url.Host == c.root.Host && !c.containsString(url.String()) {
+				c.mu.Lock()
+				c.scannedItems[url.String()] = true
+				c.mu.Unlock()
+				go c.scanUrl(&url)
+			}
+
+		case <-ctx.Done():
+			return
+		}
+		time.Sleep(50 * time.Microsecond)
+	}
+}
 
 // NewCrawler is the crawler inicialization method
 func NewCrawler(urlString string, chans Channels, parents bool) (*Crawler, error) {
@@ -55,40 +82,24 @@ func NewCrawler(urlString string, chans Channels, parents bool) (*Crawler, error
 		scannedItems: map[string]bool{},
 		scanParent:   parents,
 	}
-	crawler.urlsChan = make(chan url.URL)
+	crawler.urlsChan = make(chan url.URL, 1)
+
 	return crawler, nil
 }
 
 // Run is the crawler start method
 func (c *Crawler) Run() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func(u *url.URL) {
-		go c.scanUrl(u, &wg)
-		c.scannedItems[u.String()] = true
-		var ur url.URL
-		for {
-			ur = <-c.urlsChan
-			ur, err := c.repairUrl(&ur)
-			if err != nil {
-				continue
-			}
-			if u.Host != c.root.Host {
-				continue
-			}
-			if c.containsString(ur.String()) {
-				continue
-			}
-			c.scannedItems[ur.String()] = true
-			wg.Add(1)
-			go c.scanUrl(&ur, &wg)
-		}
-	}(c.root)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go worker(c, ctx)
+	}
+	c.urlsChan <- *c.root
 	wg.Wait()
 }
 
-func (c *Crawler) scanUrl(u *url.URL, wg *sync.WaitGroup) error {
-	defer wg.Done()
+func (c *Crawler) scanUrl(u *url.URL) error {
 	if u.String() != "" && !strings.Contains(u.String(), "javascript:") {
 		resp, err := c.client.Get(u.String())
 		if err != nil {
@@ -109,16 +120,14 @@ func (c *Crawler) scanUrl(u *url.URL, wg *sync.WaitGroup) error {
 			if err != nil {
 				return fmt.Errorf("unable to parse page body")
 			}
-			wg.Add(1)
-			go c.scanNode(doc, wg)
+			c.scanNode(doc)
 		}
 
 	}
 	return nil
 }
 
-func (c *Crawler) scanNode(n *html.Node, wg *sync.WaitGroup) {
-	defer wg.Done()
+func (c *Crawler) scanNode(n *html.Node) {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
 			if a.Key == "href" {
@@ -130,8 +139,7 @@ func (c *Crawler) scanNode(n *html.Node, wg *sync.WaitGroup) {
 		}
 	}
 	for child := n.FirstChild; child != nil; child = child.NextSibling {
-		wg.Add(1)
-		go c.scanNode(child, wg)
+		c.scanNode(child)
 	}
 }
 
@@ -162,6 +170,8 @@ func (c *Crawler) repairUrl(u *url.URL) (url.URL, error) {
 }
 
 func (c *Crawler) containsString(item string) bool {
+	c.mu.Lock()
 	_, contain := c.scannedItems[item]
+	c.mu.Unlock()
 	return contain
 }
